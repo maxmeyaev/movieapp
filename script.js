fda51f97b6026ff66835c8b628b805655c638c22
maxmeyaev <maxmeyaev05@gmail.com>
2021-11-24 18:00:23 -0500

    added dom

--- script.js
+++ script.js
@@ -4,15 +4,47 @@ const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=7202befe78
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getmovies(API_URL);
+
 async function getmovies(url){
     const res = await fetch(url); //return a promise
     const data = await res.json();
 
+    showMovies(data.results);
     console.log(data.results);
 }
-
+function showMovies(movies){
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieElem = document.createElement('div');
+        movieElem.classList.add('movie');
+        movieElem.innerHTML = 
+        `
+            <img src="${img_path + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getVote(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieElem);
+    })
+}
+function getVote(vote) {
+    if(vote >= 8){
+        return 'good';
+    } else if(vote >= 5){
+        return 'average';
+    } else {
+        return 'poor';
+    }
+}
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
 
